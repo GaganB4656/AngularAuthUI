fe7686024dc58bb6286830267c1a997ec1cf1466
Gagan B <gaganbacademics6@gmail.com>
2025-06-25 11:56:05 +0530

    SignUp Page done

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
+  type: string = 'password';
+  isText: boolean = false;
+  eyeIcon: string = 'fa-eye-slash';
 
+  hideShowPass() {
+    this.isText = !this.isText;
+    this.type = this.isText ? 'text' : 'password';
+    this.eyeIcon = this.isText ? 'fa-eye' : 'fa-eye-slash';
+  }
 }
