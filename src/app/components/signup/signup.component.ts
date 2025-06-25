9c76b0b6cfdb1bdfac0616403cd924af2e3003e3
Gagan B <gaganbacademics6@gmail.com>
2025-06-25 16:26:00 +0530

    Validations added

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,20 +1,48 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, ReactiveFormsModule, Validators, FormControl } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import ValidateForm from '../../helpers/validateform';
 
 @Component({
   selector: 'app-signup',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss',
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
   type: string = 'password';
   isText: boolean = false;
   eyeIcon: string = 'fa-eye-slash';
+  singupForm! : FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.singupForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required]],
+      username: ['', Validators.required],
+      password: ['', [Validators.required]],
+    });
+  }
 
   hideShowPass() {
     this.isText = !this.isText;
     this.type = this.isText ? 'text' : 'password';
     this.eyeIcon = this.isText ? 'fa-eye' : 'fa-eye-slash';
   }
+
+  onSignup() {
+    if (this.singupForm.valid) {
+      // send data to backend
+      console.log(this.singupForm.value);
+    } else {
+      // display toaster error message
+      ValidateForm.validateAllFormFields(this.singupForm);
+      alert("Please fill all fields correctly ⚠️");
+    }
+  }
+
 }
